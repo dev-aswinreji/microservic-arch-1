fcccd71a7104e71324e4bf34c9c04d6356885075
Aswin Reji <aswinreji.004@gmail.com>
2025-01-06 20:32:36 +0000

    fix: dockerfile in user-auth frontend modified and port exposed success

--- USER-AUTH/Backend/src/app.ts
+++ USER-AUTH/Backend/src/app.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
+import cors from "cors"
 
 const app = express();
 
+app.use(cors<Request>())
 const PORT = process.env.PORT || 4000;
 console.log(PORT, "Port is showing");
 
--- USER-AUTH/Frontend/src/App.tsx
+++ USER-AUTH/Frontend/src/App.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
 
+  const [loginData, setLoginData] = useState("no data found \n")
+
+  useEffect(() => {
+    getLoginData()
+  })
+
+  const getLoginData = async () => {
+    const result = await axios.get("http://localhost:4000/login")
+    console.log(result);
+
+    setLoginData(result.data.message)
+  }
+  console.log(loginData);
+
+
   return (
     <>
-      Hello world  
+      <p>
+        {loginData}
+      </p>
+      <h1>
+        Hello world
+      </h1>
     </>
   )
 }
